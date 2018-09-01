71064193e1be6e7f2f8f540d803c42fa36530c2e
Stephen Bowers <srb4887@gmail.com>
2018-09-01 16:50:21 -0700

    Optimize responsiveness

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -88,8 +88,8 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
-  // image.src = DBHelper.imageUrlForRestaurant(restaurant);
   image.src = `img/${restaurant.id}_lg.jpg`;
+  image.alt = restaurant.name;
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
