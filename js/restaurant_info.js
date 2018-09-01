7bb9a74b61c0a99edf2e6255e4e2af6b2d99e51c
Stephen Bowers <srb4887@gmail.com>
2018-09-01 14:25:32 -0700

    Makes images responsive

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -88,7 +88,8 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
-  image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  // image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.src = `img/${restaurant.id}_lg.jpg`;
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
