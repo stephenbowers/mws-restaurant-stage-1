8b5ec55e24336baf451196a898101a01cafce0a7
Stephen Bowers <srb4887@gmail.com>
2018-09-02 10:31:23 -0700

    feat: Add service worker

---
+++ sw.js
@@ -0,0 +1,39 @@
+let staticCacheName = 'restaurant-v1';
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(staticCacheName).then(function(cache) {
+            return cache.addAll([
+                '/',
+                'js/main.js',
+                'js/restaurant_info.js',
+                'js/dbhelper.js',
+                'img/'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName.startsWith('restaurant-') &&
+                            cacheName != staticCacheName;  
+                }).map(function(cacheName) {
+                    return cache.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) return response;
+            return fetch(event.request);
+        })
+    );
+});
